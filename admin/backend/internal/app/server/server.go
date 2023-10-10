7569793a607caaff7cccbf992f92f8b8f573081f
Vladislav <khovratovic15@yandex.ru>
2023-10-10 11:31:34 +0300

    created server

---
+++ admin/backend/internal/app/server/server.go
@@ -0,0 +1,54 @@
+package server
+
+import (
+	"admin/internal/app/api"
+	"admin/internal/app/controller"
+	"admin/internal/app/middleware/database"
+	"admin/internal/app/middleware/service"
+	"admin/internal/config"
+	"context"
+	"net/http"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/sirupsen/logrus"
+)
+
+type Server struct {
+	cfg config.Config
+	srv *http.Server
+	ctx context.Context
+}
+
+func New(cfg config.Config) Server {
+	return Server{cfg: cfg, ctx: context.Background()}
+}
+
+func (s *Server) Serve() {
+	var log logrus.Logger = *logrus.New()
+	
+	log.Info("Сервер запускается")
+
+	pool, err := pgxpool.New(s.ctx, s.cfg.DbUrl())
+	if err != nil {
+		log.Error("Ошибка подключения к базе: ", err.Error())
+	}
+
+	articleDatabase := database.NewArticle(s.ctx, pool, log)
+
+	articleService := service.NewArticle(articleDatabase)
+
+	articleController := controller.NewArticle(articleService, log)
+	cityController := controller.NewCity()
+	locationController := controller.NewLocation()
+
+	route := api.Route(articleController, cityController, locationController)
+
+	s.srv = &http.Server{
+		Addr: ":8085",
+		Handler: route,
+	}
+
+	log.Info("Сервер запущен")
+
+	s.srv.ListenAndServe()
+}
