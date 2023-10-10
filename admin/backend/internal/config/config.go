eb63cef5ad973a3e89c576e5c69fb3f36ce64205
Vladislav <khovratovic15@yandex.ru>
2023-10-10 11:31:47 +0300

    added func dburl

---
+++ admin/backend/internal/config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	Port string
+	dbHost string
+	dbPort string
+	dbUser string
+	dbPass string
+	dbName string
+}
+
+func Load() Config {
+	return Config{
+		Port: os.Getenv("port"),
+		dbHost: os.Getenv("db-host"),
+		dbPort: os.Getenv("db-port"),
+		dbUser: os.Getenv("db-user"),
+		dbPass: os.Getenv("db-pass"),
+		dbName: os.Getenv("db-name"),
+	}
+}
+
+func (c *Config) DbUrl() string {
+	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s", c.dbUser, c.dbPass,
+		c.dbHost, c.dbPort, c.dbName)
+}
