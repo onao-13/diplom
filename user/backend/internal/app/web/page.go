30479490470f197cd7af229e82157c4914000155
Vladislav <khovratovic15@yandex.ru>
2023-10-11 18:32:15 +0300

    update path

--- user/backend/internal/app/web/page.go
+++ user/backend/internal/app/web/page.go
@@ -1,21 +1,25 @@
 package web
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 type Page struct {
 	Body []byte
 }
 
 const (
-	Index     = "./html/index.html"
-	Home      = "./html/home.html"
-	Articles  = "./html/articles.html"
-	Locations = "./html/locations.html"
+	Index     = "./../frontend/html/index.html"
+	Home      = "./../frontend/html/home.html"
+	Articles  = "./../frontend/html/articles.html"
+	Locations = "./../frontend/html/locations.html"
 )
 
 func LoadPage(name string) (Page, error) {
 	body, err := os.ReadFile(name)
 	if err != nil {
+		fmt.Println(err.Error())
 		return Page{}, err
 	}
 
