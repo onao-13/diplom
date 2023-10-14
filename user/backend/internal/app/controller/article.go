c58c40ae5ae8496340463c9f519c7ce2d4063ad0
Vladislav <khovratovic15@yandex.ru>
2023-10-14 14:51:26 +0300

    update controller

--- user/backend/internal/app/controller/article.go
+++ user/backend/internal/app/controller/article.go
@@ -1,24 +1,51 @@
 package controller
 
 import (
+	"backend/internal/app/handler"
+	"backend/internal/app/middleware/service"
 	"net/http"
-	"os"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type Article struct {
-	
+	s service.Article
 }
 
-func NewArticle() Article {
-	return Article{}
+func NewArticle(s service.Article) Article {
+	return Article{s}
 }
 
-func (*Article) Get(w http.ResponseWriter, r *http.Request) {
-	data, err := os.ReadFile("./json/articles.json")
+func (a *Article) List(w http.ResponseWriter, r *http.Request) {
+	res, err := a.s.List()
 	if err != nil {
+		handler.HandlerInternalServerError(w, "Ошибка получения статей")
 		return
 	}
 
-	w.WriteHeader(200)
-	w.Write(data)
+	handler.HandleOkData(w, res)
+}
+
+func (a *Article) Get(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	ids, ok := vars["id"]
+	if !ok {
+		handler.HandleBadRequest(w, "ID пустое")
+		return
+	}
+
+	id, err := strconv.ParseInt(ids, 10, 64)
+	if err != nil {
+		handler.HandlerInternalServerError(w, "Ошибка получения ID")
+		return
+	}
+
+	res, err := a.s.Get(id)
+	if err != nil {
+		handler.HandleNotFound(w, "Статья не найдена")
+		return
+	}
+
+	handler.HandleOkData(w, res)
 }
