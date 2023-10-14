ab3c991725952d5ea9b39e6826ef3bda46bc5306
Vladislav <khovratovic15@yandex.ru>
2023-10-14 14:46:52 +0300

    upd column name and added article category table

--- deployments/init-script.sql
+++ deployments/init-script.sql
@@ -1,12 +1,12 @@
 -- HOME
 CREATE TABLE cities(
     id SERIAL PRIMARY KEY,
-    cityName VARCHAR(50) NOT NULL UNIQUE
+    name VARCHAR(50) NOT NULL UNIQUE
 );
 
 CREATE TABLE homes(
     id SERIAL PRIMARY KEY,
-    homeName VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
     price INT NOT NULL,
     homeFeatureId BIGINT NOT NULL REFERENCES homes_features(id),
     cityId BIGINT NOT NULL REFERENCES cities(id)   
@@ -15,24 +15,30 @@ CREATE TABLE homes(
 CREATE TABLE homes_features(
     homeId BIGINT NOT NULL REFERENCES homes(id),
     transportDataId BIGINT NOT NULL REFERENCES home_transport_data(id),
-    popularLocationsDataId BIGINT NOT NULL REFERENCES home_popular_locations_data(id)
+    popularLocationsId BIGINT NOT NULL REFERENCES home_popular_locations_data(id)
 );
 
 CREATE TABLE home_transport_data(
     id SERIAL PRIMARY KEY,
-    transportName VARCHAR(150) NOT NULL
+    name VARCHAR(150) NOT NULL
 );
 
 CREATE TABLE home_popular_locations_data(
     id SERIAL PRIMARY KEY,
-    popularLocationsName VARCHAR(150) NOT NULL
+    name VARCHAR(150) NOT NULL
 );
 
 -- ARTICLES
-CREATE TABLE artciles(
+CREATE TABLE articles_categories(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL
+);
+
+CREATE TABLE articles(
     id SERIAL PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
-    content VARCHAR(5000) NOT NULL
+    content VARCHAR(5000) NOT NULL,
+    categotyId BIGINT NOT NULL REFERENCES articles_categories(id),
 );
     -- imageId BIGINT NOT NULL REFERENCES images(id) 
 
