3b0ff72544b117b9800132ebe45fe7363a1beaf3
Vladislav <khovratovic15@yandex.ru>
2023-10-14 14:50:37 +0300

    added logger and context

--- user/backend/cmd/main.go
+++ user/backend/cmd/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"backend/internal/app/server"
 	"backend/internal/config"
+	"context"
+
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	var log = logrus.New()
+	ctx := context.Background()
 	c := config.Dev()
-	s := server.New(c)
+	s := server.New(c, ctx, *log)
 	s.Serve()
 }
