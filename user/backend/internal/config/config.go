8a489dae58fb6acf2f66d42d4360bed9ebf247b0
Vladislav <khovratovic15@yandex.ru>
2023-10-14 14:52:15 +0300

    updated config

--- user/backend/internal/config/config.go
+++ user/backend/internal/config/config.go
@@ -1,9 +1,31 @@
 package config
 
+import (
+	"fmt"
+	"os"
+)
+
 type Config struct {
 	Port string
+	dbUser string
+	dbPass string
+	dbHost string
+	dbPort string
+	dbName string
 }
 
 func Dev() Config {
-	return Config{"8080"}
+	return Config{
+		Port: os.Getenv("port"),
+		dbUser: os.Getenv("db-user"),
+		dbPass: os.Getenv("db-pass"),
+		dbHost: os.Getenv("db-host"),
+		dbPort: os.Getenv("db-port"),
+		dbName: os.Getenv("db-name"),
+	}
+}
+
+func (c *Config) DbUrl() string {
+	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s", c.dbUser, c.dbPass,
+		c.dbHost, c.dbPort, c.dbName)		
 }
